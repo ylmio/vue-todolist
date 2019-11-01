6cf08f60c0ffad142aa61ea01ea1439eb5f5d295
ylm <yingleiming@outlook.com>
2019-11-01 09:36:14 +0800

    引入公共样式common.css

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,37 @@
 <template>
-  <div id="app">
+  <div id="root">
     <Header></Header>
+    <List></List>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import List from './components/List.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'app',
   components: {
     Header,
+    List,
+    Footer
   }
 }
 </script>
 
 <style>
-#app {
+#root {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 6rem auto;
+  width: 80%;
+  padding: 1rem;
+  border: 1px solid #dddddd;
+  border-radius: 0.5rem;
 }
 </style>
